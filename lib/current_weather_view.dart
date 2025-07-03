a3e7b18c1db3c59eceb274951255ffaef196e5b6
lpastyle <lpastorelli@lpastyle.com>
2025-07-03 11:51:29 +0200

    Added UI part 1

---
+++ lib/current_weather_view.dart
@@ -0,0 +1,42 @@
+
+import 'package:flutter/material.dart';
+import 'package:open_weather_map_live/weather_api_response.dart';
+class CurrentWeatherView extends StatelessWidget {
+  final Forecast forecast;
+
+  const CurrentWeatherView({super.key, required this.forecast});
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Container(
+      width: size.width,
+      height: size.height / 3,
+      padding: const EdgeInsets.all(8),
+      child: Card(
+        elevation: 7,
+        child: Container (
+          padding: const EdgeInsets.all(8),
+          child: Column(
+            children: [
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // Image à venir,
+                  Text("${forecast.main.temp.toInt()}°C", style: const TextStyle(fontSize: 26))
+                ],
+              ),
+              const Spacer(),
+              Text(forecast.weather.first.description, style: const TextStyle(fontSize: 24)),
+              Text("Min:${forecast.main.tempMin.toInt()}°C - Max:${forecast.main.tempMax.toInt()}", style: const TextStyle(fontSize: 22)),
+            ],
+          ),
+        ),
+
+      ),
+
+    );
+  }
+
+}
